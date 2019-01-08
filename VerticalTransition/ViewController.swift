d7872e1e447f618e63291032c0ede3aec8a22ca6
João Aquino <joao.aquino@gingaone.com>
2019-01-08 14:36:12 -0200

    Initial Commit

---
+++ VerticalTransition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VerticalTransition
+//
+//  Created by João Aquino on 08/01/2019.
+//  Copyright © 2019 João Aquino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
